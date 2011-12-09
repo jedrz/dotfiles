88cb0892a3700832d678e87b6be1b1b19756da2e
Łukasz Jędrzejewski <lukasz892@gmail.com>
2011-12-09 20:13:36 +0100

    awesome: wallpaper_cmd fix

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -101,7 +101,7 @@ theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/defau
 theme.titlebar_maximized_button_focus_active = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
 -- You can use your own command to set your wallpaper
---theme.wallpaper_cmd = { "feh --bg-scale /home/lukasz/Obrazy/wallpaper-1282045.jpg" }
+theme.wallpaper_cmd = { "" }
 
 -- You can use your own layout icons like this:
 --[[
