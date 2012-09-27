a2c0d5b261165818dff27bc4480885421622648b
Łukasz Jędrzejewski <lukasz892@gmail.com>
2012-09-27 14:59:09 +0200

    Add icons for dzen

--- .feeddzen.py
+++ .feeddzen.py
@@ -3,6 +3,7 @@
 
 import locale
 import platform
+import os.path
 
 from feeddzen.manager import Manager
 from feeddzen.plugins import *
@@ -11,6 +12,18 @@ from feeddzen.plugins import *
 locale.setlocale(locale.LC_TIME, ('pl_PL', 'UTF-8'))
 
 is_laptop = platform.node() == 'laptop'
+
+# Icons
+icon_path = os.path.expanduser('~/.xbm-icons')
+def create_icons_dict(**kwds):
+    d = {icon_short: StaticWidget('^i({}) '.format(os.path.join(icon_path, icon_name))) \
+             for icon_short, icon_name in kwds.items()}
+    return d
+
+icons = create_icons_dict(bat='bat_full_01.xbm',
+                          clock='clock.xbm',
+                          alsa='spkr_01.xbm',
+                          mpd='play.xbm')
 
 # Create widgets.
 sep = StaticWidget(' << ')
@@ -21,32 +34,32 @@ clock = ClockWidget(
 
 alsa = AlsaWidget(
     120 if is_laptop else 31,
-    'Vol: {volume} [{state}]')
+    '{volume} [{state}]')
 
 if is_laptop:
     bat = BatteryWidget(
         39,
-        'BAT: {percentage}',
-        'BAT: {percentage} [{hours}:{minutes}:{seconds}]',
-        'BAT: {percentage} [{hours}:{minutes}:{seconds}]')
+        '{percentage}',
+        '{percentage} [{hours}:{minutes}:{seconds}]',
+        '{percentage} [{hours}:{minutes}:{seconds}]')
 
 mpd = MPDWidgetMPC(
     30,
-    'MPD: %artist% - %title%',
-    'MPD: stop')
+    '%artist% - %title%',
+    'stop')
 
 # The first item will be placed at left side.
 if is_laptop:
     widgets = [
-        bat, sep,
-        alsa, sep,
-        clock
+        icons['bat'], bat, sep,
+        icons['alsa'], alsa, sep,
+        icons['clock'], clock
     ]
 else:
     widgets = [
-        mpd, sep,
-        alsa, sep,
-        clock
+        icons['mpd'], mpd, sep,
+        icons['alsa'], alsa, sep,
+        icons['clock'], clock
     ]
 
 # x pos - to see workspaces
@@ -57,7 +70,9 @@ if is_laptop:
 else:
     w = 1920 - x * 2
 
-dzen_command = 'dzen2 -bg #000 -x {} -w {}'.format(x, w)
+font = 'Terminus-9'
+
+dzen_command = 'dzen2 -bg #000 -y 2 -x {} -w {} -fn {}'.format(x, w, font)
 
 manager = Manager(widgets, dzen_command)
 manager.start()
